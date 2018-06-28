0d3dfaaea89ff9d96a492bab0187fafc60f7b5f3
Marius Orcsik <marius.orcsik@rocket-internet.de>
2018-06-28 16:29:16 +0200

    Forgot to add the parent redirect intermediary page

---
+++ parent.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/astaxie/beego/orm"
+)
+
+// handleMain serves /parent/{hash} request
+func (l *littr) handleParent(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	db, err := orm.GetDB("default")
+	if err != nil {
+		l.handleError(w, r, err)
+		return
+	}
+
+	sel := `select par.submitted_at, par.key from content_items par 
+		inner join content_items cur on subltree(cur.Path, nlevel(cur.Path)-1, nlevel(cur.Path)) <@ par.Key::ltree
+			where cur.Key ~* $1 and par.Key ~* $2`
+	rows, err := db.Query(sel, vars["hash"], vars["parent"])
+	if err != nil {
+		l.handleError(w, r, err)
+		return
+	}
+	for rows.Next() {
+		p := Content{}
+		err = rows.Scan(&p.SubmittedAt, &p.Key)
+		if err != nil {
+			l.handleError(w, r, err)
+			return
+		}
+
+		url := p.PermaLink()
+		http.Redirect(w, r, url, http.StatusMovedPermanently)
+	}
+}
