c73a6772c1b74ff87f951bd6921f9b8626ccba11
Marius Orcsik <marius.orcsik@rocket-internet.de>
2018-07-02 17:42:05 +0200

    Adding fugly functionality to change white/dark theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,21 @@ $( document ).ready(function() {
 
     var _User = JSON.parse($("#currentUser").html());
     console.debug(_User);
+
+    var isInverted = Cookies.get("inverted") || false;
+
+    if (isInverted) {
+        $(":root").toggleClass("inverted");
+    }
+
+    $("#act-invert").click(function(e) {
+        $(":root").toggleClass("inverted");
+        if (isInverted) {
+            Cookies.remove("inverted");
+        } else {
+            Cookies.set("inverted", true);
+        }
+        e.preventDefault();
+        e.stopPropagation();
+    });
 });
