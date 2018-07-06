db644f5cff9ebc813534c9a6cb248511eb324361
Marius Orcsik <marius.orcsik@rocket-internet.de>
2018-07-06 20:21:47 +0200

    Exporting variables from .env one by one instead of sourcing the file

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-export GOPATH=$(pwd):$GOPATH
-. .env
+IFS=$'\r\n'
+GLOBIGNORE='*'
 
-go run ./app/*.go
+__env=($(<.env))
+for i in ${__env[@]}; do
+    name=${i%=*}
+    quot=${i#*=}
+    value=${quot//\"}
+    export ${name}=${value}
+done
+go build -o littr main.go && ./littr
