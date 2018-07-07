354c21a433e5e5b555fa803249fe8c684cbe287c
Marius Orcsik <marius.orcsik@rocket-internet.de>
2018-07-07 21:24:25 +0200

    Adding AP articles for the account

--- api/accounts.go
+++ api/accounts.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/mariusor/littr.go/models"
@@ -14,32 +15,52 @@ import (
 
 var CurrentAccount *models.Account
 
-func loadAccount(handle string) (*models.Account, int64, error) {
+func loadAccount(handle string) (*models.Account, *[]models.Content, error) {
 	a := models.Account{}
-	var itemCount int64
-	selAcct := `select "accounts"."id", "accounts"."key", "handle", "email", "accounts"."score", "created_at", "accounts"."updated_at",
-  "accounts"."metadata", "accounts"."flags",
-  count("content_items"."id") as "submission_count" from "accounts"
-left join "content_items" on "content_items"."submitted_by" = "accounts"."id"
- where "handle" = $1 group by "accounts"."id"`
+	selAcct := `select "id", "key", "handle", "email", "score", "created_at", "updated_at", "metadata", "flags" from "accounts" where "handle" = $1`
 	rows, err := Db.Query(selAcct, handle)
 	if err != nil {
-		return nil, 0, err
+		return nil, nil, err
 	}
 	for rows.Next() {
-		err = rows.Scan(&a.Id, &a.Key, &a.Handle, &a.Email, &a.Score, &a.CreatedAt, &a.UpdatedAt, &a.Metadata, &a.Flags, &itemCount)
+		err = rows.Scan(&a.Id, &a.Key, &a.Handle, &a.Email, &a.Score, &a.CreatedAt, &a.UpdatedAt, &a.Metadata, &a.Flags)
 		if err != nil {
-			return nil, 0, err
+			return nil, nil, err
 		}
 	}
-	return &a, itemCount, nil
+
+	items := make([]models.Content, 0)
+	selC := `select "content_items"."id", "content_items"."key", "mime_type", "data", "title", "content_items"."score", 
+			"submitted_at", "content_items"."flags", "content_items"."metadata", "accounts"."handle" f from "content_items" 
+			left join "accounts" on "accounts"."id" = "content_items"."submitted_by" 
+			where "submitted_by" = $1 order by "submitted_at" desc`
+	{
+		rows, err := Db.Query(selC, a.Id)
+		if err != nil {
+			return nil, nil, err
+		}
+		for rows.Next() {
+			p := models.Content{}
+			err = rows.Scan(&p.Id, &p.Key, &p.MimeType, &p.Data, &p.Title, &p.Score, &p.SubmittedAt, &p.Flags, &p.Metadata, &p.Handle)
+			if err != nil {
+				return nil, nil, err
+			}
+			p.SubmittedBy = a.Id
+			items = append(items, p)
+		}
+	}
+	if err != nil {
+		log.Print(err)
+	}
+
+	return &a, &items, nil
 }
 
 // GET /api/accounts/{handle}
 func HandleAccount(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 
-	a, _, err := loadAccount(vars["handle"])
+	a, items, err := loadAccount(vars["handle"])
 	if err != nil {
 		HandleError(w, r, http.StatusInternalServerError, err)
 		return
@@ -53,6 +74,20 @@ func HandleAccount(w http.ResponseWriter, r *http.Request) {
 	p.Name["en"] = a.Handle
 	p.PreferredUsername["en"] = a.Handle
 	p.URL = fmt.Sprintf(a.PermaLink())
+
+	//p.Outbox = ap.OutboxStream(ap.Outbox(ap.OrderedCollection{ID: ap.ObjectID("outbox")}))
+	p.Outbox = ap.OrderedCollectionNew(ap.ObjectID("outbox"))
+	for _, item := range *items {
+		note := ap.ObjectNew(ap.ObjectID(item.Hash()), ap.ArticleType)
+		note.Content["en"] = string(item.Data)
+		if item.Title != nil {
+			note.Name["en"] = string(item.Title)
+		}
+		note.Published = item.SubmittedAt
+		note.Updated = item.UpdatedAt
+		note.URL = item.PermaLink()
+		p.Outbox.Append(note)
+	}
 
 	json.Ctx = GetContext()
 	j, err := json.Marshal(p)
