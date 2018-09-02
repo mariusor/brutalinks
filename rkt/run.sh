11ec58facb78f43b13b70b1943b80bd701694cc1
Marius Orcsik <marius@habarnam.ro>
2018-09-02 20:27:35 +0200

    Removed rckt related resources

--- rkt/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-__version=$(printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)")
-
-rkt --debug --insecure-options=image \
-    run \
-    littr-me-postgres-${__version}-linux-amd64.aci --volume=data,kind=host,source=/tmp/data \
-    littr-me-nginx-${__version}-linux-amd64.aci --net=host \
-    littr-me-app-${__version}-linux-amd64.aci
