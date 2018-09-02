a5f611736fecad0bc38b263e5537948bd4d1d7e8
Marius Orcsik <marius@habarnam.ro>
2018-09-02 20:59:28 +0200

    Initial set-up for generating ssh-keys for users

---
+++ cli/keys/README.md
@@ -0,0 +1,7 @@
+## Generator for default user ssh-keys
+
+Your .env file should contain at least these entries:
+
+    DB_NAME=littr
+    DB_USER=littr
+    DB_PASSWORD=SuperSecret,SecretPassword
---
+++ cli/keys/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/x509"
+	"database/sql"
+	"flag"
+	"fmt"
+	"github.com/mariusor/littr.go/models"
+	log "github.com/sirupsen/logrus"
+	"math/rand"
+	"os"
+)
+
+func init() {
+	dbPw := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbUser := os.Getenv("DB_USER")
+
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", dbUser, dbPw, dbName)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Print(err)
+	}
+
+	models.Db = db
+}
+func main() {
+	var handle string
+	var seed int64
+	flag.StringVar(&handle, "handle", "", "the content key to update votes for")
+	flag.Int64Var(&seed, "seed", 0, "the seed used for the random number generator in key creation")
+	flag.Parse()
+
+	loader := models.LoaderService{}
+	acct, err := loader.LoadAccount(models.LoadAccountFilter{
+		Handle: handle,
+	})
+	if err == nil {
+		m := acct.Metadata
+
+		if seed != 0 {
+			if privKey, err := ecdsa.GenerateKey(elliptic.P224(), rand.New(rand.NewSource(seed))); err == nil {
+
+				pub, errPub := x509.MarshalPKIXPublicKey(privKey.PublicKey)
+				priv, errPrv := x509.MarshalECPrivateKey(privKey)
+				if errPub == nil && errPrv == nil {
+					m.Key = &models.SSHKey{
+						Id:      "id-ecdsa",
+						Public:  pub,
+						Private: priv,
+					}
+
+					loader.SaveAccount()
+				} else if errPub != nil {
+					log.Error(errPub)
+				} else {
+					log.Error(errPrv)
+				}
+			} else {
+				log.Error(err)
+			}
+		}
+	} else {
+		log.Error(err)
+	}
+}
