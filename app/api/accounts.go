ebec7280647926d56577265b866bfb67d8c36885
Marius Orcsik <marius@habarnam.ro>
2018-10-11 17:46:00 +0200

    Remove followers/following collections as I'm not sure if I'll use them ever

--- app/api/accounts.go
+++ app/api/accounts.go
@@ -101,28 +101,14 @@ func loadAPPerson(a models.Account) *Person {
 
 	out := ap.OutboxNew()
 	out.ID = BuildCollectionID(a, new(ap.Outbox))
-	//if len(a.Handle) > 0 {
-	//	out.AttributedTo = as.URI(p.ID)
-	//}
 	p.Outbox = out
 
 	in := ap.InboxNew()
 	in.ID = BuildCollectionID(a, new(ap.Inbox))
 	p.Inbox = in
 
-	followers := ap.Followers{}
-	followers.ID = BuildCollectionID(a, new(ap.Followers))
-	p.Followers = followers
-
-	following := ap.Following{}
-	following.ID = BuildCollectionID(a, new(ap.Following))
-	p.Following = following
-
 	liked := ap.LikedNew()
 	liked.ID = BuildCollectionID(a, new(ap.Liked))
-	//if len(a.Handle) > 0 {
-	//	liked.AttributedTo = as.URI(p.ID)
-	//}
 	p.Liked = liked
 
 	p.URL = as.IRI(frontend.AccountPermaLink(a))
@@ -211,12 +197,15 @@ func HandleAccount(w http.ResponseWriter, r *http.Request) {
 	//	Logger.WithFields(log.Fields{}).Errorf("could not load Account from Context")
 	//}
 	p := loadAPPerson(a)
-	p.Outbox = as.IRI(*p.Outbox.GetID())
-	p.Liked = as.IRI(*p.Liked.GetID())
-	p.Inbox = as.IRI(*p.Inbox.GetID())
-	p.Followers = as.IRI(*p.Followers.GetID())
-	p.Following = as.IRI(*p.Following.GetID())
-
+	if p.Outbox != nil {
+		p.Outbox = as.IRI(*p.Outbox.GetID())
+	}
+	if p.Liked != nil {
+		p.Liked = as.IRI(*p.Liked.GetID())
+	}
+	if p.Inbox != nil {
+		p.Inbox = as.IRI(*p.Inbox.GetID())
+	}
 	p.Endpoints = as.Endpoints{SharedInbox: as.IRI(fmt.Sprintf("%s/api/inbox", app.Instance.BaseURL))}
 
 	p.Summary = make(as.NaturalLanguageValue, 1)
