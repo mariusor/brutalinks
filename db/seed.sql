7547a63359ca32abd5664a7090de2540d41f5909
Marius Orcsik <marius@habarnam.ro>
2018-10-27 22:45:53 +0200

    Recalculated hashes for the seed file

--- db/seed.sql
+++ db/seed.sql
@@ -1,7 +1,7 @@
 -- name: add-account-system
 insert into accounts (id, key, handle, email) values (
     -1,
-    '29fc2269252dd76fa7e4b6d193f51a3f3cd21fdf30e44f34ec138d7e803cf0c3',
+    'dc6f5f5bf55bc1073715c98c69fa7ca8',
     'system',
     'system@localhost'
 );
@@ -9,14 +9,14 @@ insert into accounts (id, key, handle, email) values (
 -- name: add-account-anonymous
 insert into accounts (id, key, handle, email) values (
     0,
-    '77b7b7215e8d78452dc40da9efbb65fdc918c757844387aa0f88143762495c6b',
+    'eacff9ddf379bd9fc8274c5a9f4cae08',
     'anonymous',
     'anonymous@localhost'
 );
 
 -- name: add-item-about
 insert into content_items (key, mime_type, title, data, submitted_by) values (
-    'cb615f8863b197b86a08354911b93c0fc3d365061a83bb6482f8ac67c871d192',
+    '738e6ef3a756a56a906ecd3409270631',
     'text/html',
     'about littr.me',
     '<p>' ||
