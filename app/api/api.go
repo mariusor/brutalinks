2945633e09d3141bc75b9d5ddf5897ac09ff183b
Marius Orcsik <marius@habarnam.ro>
2018-11-01 10:59:44 +0100

    Fix jsonld score context uri

--- app/api/api.go
+++ app/api/api.go
@@ -84,7 +84,7 @@ func GetContext() j.Context {
 	return j.Context{
 		{IRI: j.IRI(as.ActivityBaseURI)},
 		{IRI: j.IRI("https://w3id.org/security/v1")},
-		{j.Term("score"), j.IRI(fmt.Sprintf("%s/ns/#score", app.Instance.BaseURL))},
+		{j.Term("score"), j.IRI(fmt.Sprintf("%s/ns#score", app.Instance.BaseURL))},
 	}
 }
 
