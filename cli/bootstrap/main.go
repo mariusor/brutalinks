a57c353481c8d180ca5798fb07249286081da3b8
Marius Orcsik <marius@habarnam.ro>
2019-01-15 11:34:23 +0100

    Added waiting for db mechanism before running queries
    Some code cleanup

--- cli/bootstrap/main.go
+++ cli/bootstrap/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/mariusor/littr.go/app/cmd"
 	"github.com/mariusor/littr.go/app/log"
+	"net"
 	"os"
 	"strings"
 	"time"
@@ -17,7 +18,7 @@ import (
 
 func dbConnection(dbHost string, dbUser string, dbPw string, dbName string) (*sql.DB, error) {
 	if dbUser == "" && dbPw == "" {
-		err := errors.NewErr("missing user and/or pw")
+		err := errors.Forbiddenf("missing user and/or pw")
 		if !cmd.E(err) {
 			os.Exit(1)
 		}
@@ -30,23 +31,29 @@ func dbConnection(dbHost string, dbUser string, dbPw string, dbName string) (*sq
 	connStr := fmt.Sprintf("host=%s user=%s%s dbname=%s sslmode=disable", dbHost, dbUser, pw, dbName)
 	db, err := sql.Open("postgres", connStr)
 	if err == nil {
-		quit := make(chan bool)
-		sl, _ := time.ParseDuration("50ms")
-		ticker := time.NewTicker(5 * sl)
-		go func(c *sql.DB) {
-			for {
-				select {
-				case <-ticker.C:
-					// do stuff
-					if c.Ping() == nil {
-						quit <- true
+		//ticker := time.NewTicker(500 * time.Millisecond)
+		cnt := 0
+		for {
+			if err := db.Ping(); err == nil {
+				if cnt > 0 {
+					fmt.Printf("\n")
+				}
+				return db, nil
+			} else {
+				if t, ok := err.(*net.OpError); ok {
+					cnt++
+					if cnt % 10 == 0 {
+						fmt.Printf(".")
 					}
-				case <-quit:
-					ticker.Stop()
-					return
+					if cnt == (720 - 22) || cnt % 720 == 0 {
+						fmt.Printf("\n")
+					}
+					time.Sleep(100 * time.Millisecond)
+				} else {
+					return db, t
 				}
 			}
-		}(db)
+		}
 	}
 	return nil, err
 }
@@ -73,59 +80,67 @@ func main() {
 	if dbHost == "" {
 		dbHost = os.Getenv("DB_HOST")
 	}
+	if len(dbRootUser) == 0 {
+		dbRootUser = "postgres"
+	}
+	dbRootName := "postgres"
 
 	var dot *dotsql.DotSql
-	if len(dbRootUser) != 0 {
-		dbRootName := "postgres"
-		rootDB, err := dbConnection(dbHost, dbRootUser, dbRootPw, dbRootName)
-		rr := errors.Annotate(err, "connection failed")
-		if !cmd.E(rr) {
-			os.Exit(1)
-		}
-		if err := rootDB.Ping(); cmd.E(err) {
-			os.Exit(1)
-		}
+	rootDB, err := dbConnection(dbHost, dbRootUser, dbRootPw, dbRootName)
+	if !cmd.E(errors.Annotate(err, "connection failed")) {
+		os.Exit(1)
+	}
+	defer rootDB.Close()
 
-		defer rootDB.Close()
+	// create new role and db with root user in root database
+	dot, err = dotsql.LoadFromFile("./db/create_role.sql")
+	if !cmd.E(errors.Annotatef(err, "unable to load file")) {
+		os.Exit(1)
+	}
+	s1, _ := dot.Raw("create-role-with-pass")
+	_, err = rootDB.Exec(fmt.Sprintf(s1, dbUser, strings.Trim(dbPw, "'")))
+	if !cmd.E(errors.Annotatef(err, "query: %s", s1)) {
+		os.Exit(1)
+	}
 
-		// create new role and db with root user in root database
-		dot, err = dotsql.LoadFromFile("./db/create_role.sql")
-		s1, _ := dot.Raw("create-role-with-pass")
-		_, err = rootDB.Exec(fmt.Sprintf(s1, dbUser, strings.Trim(dbPw, "'")))
-		cmd.E(errors.Annotatef(err, "query: %s", s1))
+	s2, _ := dot.Raw("create-db-for-role")
+	_, err = rootDB.Exec(fmt.Sprintf(s2, dbName, dbUser))
+	if !cmd.E(errors.Annotatef(err, "query: %s", s2)) {
+		os.Exit(1)
+	}
 
-		s2, _ := dot.Raw("create-db-for-role")
-		_, err = rootDB.Exec(fmt.Sprintf(s2, dbName, dbUser))
-		cmd.E(errors.Annotatef(err, "query: %s", s2))
+	// root user, but our new created db
+	db, err := dbConnection(dbHost, dbRootUser, dbRootPw, dbName)
+	if !cmd.E(errors.Annotate(err, "connection failed")) {
+		os.Exit(1)
+	}
+	defer db.Close()
 
-		// root user, but our new created db
-		db, err := dbConnection(dbHost, dbRootUser, dbRootPw, dbName)
-		if !cmd.E(errors.Annotate(err, "connection failed")) {
-			os.Exit(1)
-		}
-		if err := db.Ping(); cmd.E(err) {
-			os.Exit(1)
-		}
-
-		defer db.Close()
-		dot, err = dotsql.LoadFromFile("./db/extensions.sql")
-		_, err = dot.Exec(db, "extension-pgcrypto")
-		s1, _ = dot.Raw("extension-pgcrypto")
-		cmd.E(errors.Annotatef(err, "query: %s", s1))
-		_, err = dot.Exec(db, "extension-ltree")
-		s2, _ = dot.Raw("extension-ltree")
-		cmd.E(errors.Annotatef(err, "query: %s", s2))
+	dot, err = dotsql.LoadFromFile("./db/extensions.sql")
+	if !cmd.E(errors.Annotatef(err, "unable to load file")) {
+		os.Exit(1)
+	}
+	_, err = dot.Exec(db, "extension-pgcrypto")
+	s1, _ = dot.Raw("extension-pgcrypto")
+	if !cmd.E(errors.Annotatef(err, "query: %s", s1)) {
+		os.Exit(1)
+	}
+	_, err = dot.Exec(db, "extension-ltree")
+	s2, _ =  dot.Raw("extension-ltree")
+	if !cmd.E(errors.Annotatef(err, "query: %s", s2)) {
+		os.Exit(1)
 	}
 
 	// newly created user in newly created database
-	db, err := dbConnection(dbHost, dbUser, dbPw, dbName)
+	db, err = dbConnection(dbHost, dbUser, dbPw, dbName)
 	if !cmd.E(err) {
 		os.Exit(1)
 	}
 	defer db.Close()
-	// create new role and db with root user in root database
 	dot, err = dotsql.LoadFromFile("./db/init.sql")
-	cmd.E(errors.Annotate(err, "unable to load file"))
+	if !cmd.E(errors.Annotatef(err, "unable to load file")) {
+		os.Exit(1)
+	}
 	drop, _ := dot.Raw("drop-tables")
 	_, err = db.Exec(fmt.Sprintf(drop))
 	cmd.E(errors.Annotatef(err, "query: %s", drop))
@@ -148,7 +163,9 @@ func main() {
 
 	if seed {
 		dot, err = dotsql.LoadFromFile("./db/seed.sql")
-		cmd.E(errors.Annotate(err, "unable to load file"))
+		if !cmd.E(errors.Annotatef(err, "unable to load file")) {
+			os.Exit(1)
+		}
 
 		sysAcct, _ := dot.Raw("add-account-system")
 		_, err = db.Exec(fmt.Sprintf(sysAcct))
