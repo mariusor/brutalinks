09889f842d38b963dbee8e79ac06dc0d36aa1daf
Marius Orcsik <marius@habarnam.ro>
2019-01-21 10:33:45 +0100

    Forgot to add the interfaces

--- app/activitypub/activitypub.go
+++ app/activitypub/activitypub.go
@@ -9,6 +9,26 @@ import (
 	as "github.com/go-ap/activitypub.go/activitystreams"
 )
 
+// ValidationErrors is an aggregated error interface that allows
+// a Validator implementation to return all possible errors.
+type ValidationErrors interface {
+	error
+	Errors() []error
+	Add(error)
+}
+
+// Validator is the interface that needs to be implemented by objects that
+// provide a validation mechanism for incoming ActivityPub Objects or IRIs
+// against an external set of rules.
+type Validator interface {
+	Validate(receiver as.IRI, incoming as.Item) (bool, ValidationErrors)
+}
+type defaultValidator struct {}
+
+func (v defaultValidator) Validate (receiver as.IRI, incoming as.Item) (bool, ValidationErrors) {
+	return true, nil
+}
+
 // PublicKey holds the ActivityPub compatible public key data
 type PublicKey struct {
 	ID           as.ObjectID     `jsonld:"id,omitempty"`
