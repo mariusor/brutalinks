99b8d9efddffa08406db962bda7385ae83a2f82c
Marius Orcsik <marius@habarnam.ro>
2019-02-10 13:40:09 +0100

    Revert "Use Hashes type for []Hash slices"
    I keep forgetting that []Hash is correctly encoded by qstring to uri
    variables, while Hashes is not
    
    This reverts commit 6ce1f8b0346a766a3d0cc20404ef15631d373aa8.

--- app/loader.go
+++ app/loader.go
@@ -87,9 +87,9 @@ func (h Hashes) String() string {
 }
 
 type LoadVotesFilter struct {
-	ItemKey              Hashes    `qstring:"hash,omitempty"`
+	ItemKey              []Hash    `qstring:"hash,omitempty"`
 	Type                 VoteTypes `qstring:"type,omitempty"`
-	AttributedTo         Hashes    `qstring:"attributedTo,omitempty"`
+	AttributedTo         []Hash    `qstring:"attributedTo,omitempty"`
 	SubmittedAt          time.Time `qstring:"submittedAt,omitempty"`
 	SubmittedAtMatchType MatchType `qstring:"submittedAtMatchType,omitempty"`
 	Page                 int       `qstring:"page,omitempty"`
@@ -97,7 +97,7 @@ type LoadVotesFilter struct {
 }
 
 type LoadItemsFilter struct {
-	Key                  Hashes     `qstring:"hash,omitempty"`
+	Key                  []Hash     `qstring:"hash,omitempty"`
 	MediaType            []MimeType `qstring:"mediaType,omitempty"`
 	AttributedTo         []Hash     `qstring:"attributedTo,omitempty"`
 	InReplyTo            []string   `qstring:"inReplyTo,omitempty"`
