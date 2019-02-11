66328bcfe65ee1415e48d818cd402127fa8f521f
Marius Orcsik <marius@habarnam.ro>
2019-02-11 13:04:47 +0100

    Adding some assertions for node info test

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -1,12 +1,15 @@
 package tests
 
 import (
+	"encoding/json"
 	"fmt"
 	"golang.org/x/xerrors"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"testing"
+	as "github.com/go-ap/activitystreams"
 )
 
 // UserAgent value that the client uses when performing requests
@@ -30,8 +33,8 @@ func execReq(url string, met string, body io.Reader) ([]byte, error) {
 		return nil, xerrors.Errorf("Error: invalid HTTP response %d, expected %d", resp.StatusCode, http.StatusOK)
 	}
 	b := make([]byte, 0)
-	if c, err := resp.Body.Read(b); err != nil {
-		return nil, xerrors.Errorf("Error: invalid HTTP body! Read %d bytes %s", c, b)
+	if b, err = ioutil.ReadAll(resp.Body); err != nil {
+		return nil, xerrors.Errorf("Error: invalid HTTP body! Read %d bytes %s", len(b), b)
 	}
 	return b, nil
 }
@@ -39,10 +42,55 @@ func execReq(url string, met string, body io.Reader) ([]byte, error) {
 func Test_GETNodeInfo(t *testing.T) {
 	apiURL := os.Getenv("API_URL")
 
+	host := os.Getenv("HOSTNAME")
+
+	testId := fmt.Sprintf("http://%s/api/self", host)
+	testUrl := fmt.Sprintf("http://%s", host)
+	testOutbox := fmt.Sprintf("%s/api/self/outbox", testUrl)
+	testInbox := fmt.Sprintf("%s/api/self/inbox", testUrl)
+	testAuthor := "https://github.com/mariusor"
+
 	url := fmt.Sprintf("%s/self", apiURL)
-	if b, err := execReq(url, http.MethodGet, nil); err != nil {
+	var b []byte
+	var err error
+	if b, err = execReq(url, http.MethodGet, nil); err != nil {
 		t.Errorf("Error %s", err)
-	} else {
-		t.Logf("OK %s", b)
 	}
+	test := make(map[string]interface{})
+	if err = json.Unmarshal(b, &test); err != nil {
+		t.Errorf("Error unmarshal: %s", err)
+	}
+	for key, val := range test {
+		if key == "id" {
+			if val != testId {
+				t.Errorf("Invalid id, %s expected %s", val, testId)
+			}
+		}
+		if key == "type" {
+			if val != string(as.ServiceType) {
+				t.Errorf("Invalid type, %s expected %s", val, as.ServiceType)
+			}
+		}
+		if key == "url" {
+			if val != testUrl {
+				t.Errorf("Invalid url, %s expected %s", val, testUrl)
+			}
+		}
+		if key == "outbox" {
+			if val != testOutbox {
+				t.Errorf("Invalid outbox url, %s expected %s", val, testOutbox)
+			}
+		}
+		if key == "inbox" {
+			if val != testInbox {
+				t.Errorf("Invalid inbox url, %s expected %s", val, testInbox)
+			}
+		}
+		if key == "attributedTo" {
+			if val != testAuthor {
+				t.Errorf("Invalid author, %s expected %s", val, testAuthor)
+			}
+		}
+	}
+	t.Logf("%#v", test)
 }
