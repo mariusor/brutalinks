96538a4cb45eb497cd4b05a6df82c47841549260
Marius Orcsik <marius@habarnam.ro>
2019-04-22 21:48:45 +0200

    Add private/public keys load at run time

--- tests/activitypub_s2s_test.go
+++ tests/activitypub_s2s_test.go
@@ -10,19 +10,10 @@ import (
 	"testing"
 )
 
-var lpubblock, _ = pem.Decode([]byte("-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6o5aCJjzpWRmdj27eGE+DIdC96zSTrPL6lnelZrCNSCcfMC9wZVhMwLLNlSCV1EURDMYs9fla7Hjfqqa706TC3EI6q2n1rPcLd4Cgvto6rA+luiXQGRNQmabXxVQBdn81oxRNv+M41g4ZhPkK7px5pHG+U6oU5uPu8m3fLqQpMjVHitiaou8ABRoLN6s5aAattMVULh/Ma33tqDDwt2+mDui44x9ge4dPrtP3koZZ+uiiLx+9hqG6Oaa8BrsujLrHHCLiwPaHmAacybLCjROvoJUB1mxGdHaaBoh87sb0sX7KmRDVVobkd3q8MI4zektj4N8GDoNUkukuh1mALrGjQIDAQAB\n-----END PUBLIC KEY-----"))
-var lpub, _ = x509.ParsePKIXPublicKey(lpubblock.Bytes)
-
-var prv64 = os.Getenv("TEST_PRIVATE_KEY")
-var lprvblock, _ = pem.Decode([]byte("-----BEGIN PRIVATE KEY-----\n"+prv64+"\n-----END PRIVATE KEY-----"))
-var lprv, _ = x509.ParsePKCS8PrivateKey(lprvblock.Bytes)
-
 var littrAcct = testAccount{
 	id:         "https://littr.me/api/self/following/64e55785250f59637e7f578167e2c112",
 	Hash:       "64e55785250f59637e7f578167e2c112",
 	Handle:     "0lpbm",
-	publicKey:  lpub.(*rsa.PublicKey),
-	privateKey: lprv.(*rsa.PrivateKey),
 }
 
 var S2STests = testPairs{
@@ -79,5 +70,17 @@ var S2STests = testPairs{
 }
 
 func Test_S2SRequests(t *testing.T) {
+	var lpubblock, _ = pem.Decode([]byte("-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6o5aCJjzpWRmdj27eGE+DIdC96zSTrPL6lnelZrCNSCcfMC9wZVhMwLLNlSCV1EURDMYs9fla7Hjfqqa706TC3EI6q2n1rPcLd4Cgvto6rA+luiXQGRNQmabXxVQBdn81oxRNv+M41g4ZhPkK7px5pHG+U6oU5uPu8m3fLqQpMjVHitiaou8ABRoLN6s5aAattMVULh/Ma33tqDDwt2+mDui44x9ge4dPrtP3koZZ+uiiLx+9hqG6Oaa8BrsujLrHHCLiwPaHmAacybLCjROvoJUB1mxGdHaaBoh87sb0sX7KmRDVVobkd3q8MI4zektj4N8GDoNUkukuh1mALrGjQIDAQAB\n-----END PUBLIC KEY-----"))
+	var lpub, _ = x509.ParsePKIXPublicKey(lpubblock.Bytes)
+	var prv64 = os.Getenv("TEST_PRIVATE_KEY")
+	var lprvblock, _ = pem.Decode([]byte("-----BEGIN PRIVATE KEY-----\n"+prv64+"\n-----END PRIVATE KEY-----"))
+	var lprv, _ = x509.ParsePKCS8PrivateKey(lprvblock.Bytes)
+
+	if os.Getenv("TEST_PRIVATE_KEY") == "" {
+		t.Skipf("Unable to load private key")
+	} else {
+		littrAcct.privateKey = lprv.(*rsa.PrivateKey)
+		littrAcct.publicKey =  lpub.(*rsa.PublicKey)
+	}
 	testSuite(t, S2STests)
 }
