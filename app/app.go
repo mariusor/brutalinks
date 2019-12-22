4a68add46cc2e362509128e0062681aa87a59433
Marius Orcsik <marius@habarnam.ro>
2019-12-22 21:06:57 +0100

    Revert "Removed config option for enabling anonymous commenting"
    This reverts commit ad4a1564e0edb8b82233204d5047a9c8edfee4c9.

--- app/app.go
+++ app/app.go
@@ -73,6 +73,7 @@ type Config struct {
 	DB                         backendConfig
 	ES                         backendConfig
 	Redis                      backendConfig
+	AnonymousCommentingEnabled bool
 	SessionsEnabled            bool
 	VotingEnabled              bool
 	DownvotingEnabled          bool
@@ -264,6 +265,8 @@ func loadEnv(l *Application) (bool, error) {
 	l.Config.SessionsEnabled = !sessionsDisabled
 	userCreationDisabled, _ := strconv.ParseBool(os.Getenv("DISABLE_USER_CREATION"))
 	l.Config.UserCreatingEnabled = !userCreationDisabled
+	anonymousCommentingDisabled, _ := strconv.ParseBool(os.Getenv("DISABLE_ANONYMOUS_COMMENTING"))
+	l.Config.AnonymousCommentingEnabled = !anonymousCommentingDisabled
 	userFollowingDisabled, _ := strconv.ParseBool(os.Getenv("DISABLE_USER_FOLLOWING"))
 	l.Config.UserFollowingEnabled = !userFollowingDisabled
 
