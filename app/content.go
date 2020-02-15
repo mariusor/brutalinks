3a60e3596bdaec0225da4fc5e08be653640409c9
Marius Orcsik <marius@habarnam.ro>
2020-02-15 15:02:08 +0100

    Adding convenience ways to get items and follows from a renderable list

--- app/content.go
+++ app/content.go
@@ -88,6 +88,28 @@ type Renderable interface {
 	Type() RenderType
 }
 
+type RenderableList []Renderable
+
+func (r RenderableList) Items() ItemCollection {
+	items := make(ItemCollection, 0)
+	for _, ren := range r {
+		if it, ok := ren.(*Item); ok {
+			items = append(items, *it)
+		}
+	}
+	return items
+}
+
+func (r RenderableList) Follows() FollowRequests {
+	follows := make(FollowRequests, 0)
+	for _, ren := range r {
+		if it, ok := ren.(*FollowRequest); ok {
+			follows = append(follows, *it)
+		}
+	}
+	return follows
+}
+
 func (f FollowRequest) Type() RenderType {
 	return Follow
 }
