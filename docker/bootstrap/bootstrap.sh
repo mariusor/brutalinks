5e82a316576926d10204c3b7ca5f9e09bf511262
Marius Orcsik <marius@habarnam.ro>
2020-08-02 22:08:00 +0200

    Don't output anything in the bootstrapping process if env variables are not set

--- docker/bootstrap/bootstrap.sh
+++ docker/bootstrap/bootstrap.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-echo "# bootstrapped application $(date -u -Iseconds)" >> .env
-# create storage
-ctl bootstrap
 
 test -z "${OAUTH_PW}" && exit 1
 test -z "${ADMIN_PW}" && exit 1
 test -z "${HOSTNAME}" && exit 1
+
+echo "# bootstrapped application $(date -u -Iseconds)" >> .env
+# create storage
+ctl bootstrap
 
 # add oauth2 client for littr.me
 echo OAUTH_KEY=$(setupkeys.sh "${OAUTH_PW}" "${HOSTNAME}" "${ADMIN_PW}" | grep Client | tail -1 | awk '{print $3}') >> .env
