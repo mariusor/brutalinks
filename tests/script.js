e980f22a30bdea700892502dae34d1d3edeef456
Marius Orcsik <marius@habarnam.ro>
2022-06-25 11:16:46 +0200

    Fix test script

--- tests/script.js
+++ tests/script.js
@@ -25,12 +25,13 @@ export const options = {
     },
 }
 
+const BASE_URL = "https://brutalinks.git";
 const PASSWORD = 'Sup3rS3cretS3cr3tP4ssW0rd!';
 
 export function setup() {
-    for (int i = 1; i <= 10; i++) {
-        const res = http.get(`${BASE_URL}/register/`);
-        check(res, { 'created user': (r) => r.status === 200 });
+    for (let i = 1; i <= 10; i++) {
+        let response = http.get(`${BASE_URL}/register/`);
+        check(response, { 'created user': (r) => r.status === 200 });
 
         response = response.submitForm({
             formSelector: 'form',
@@ -56,44 +57,44 @@ export function regular_browsing() {
     let response;
 
     group('Homepage', function () {
-        response = http.get(`https://${BASE_URL}/`, {
+        response = http.get(`${BASE_URL}/`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
 
-        response = http.get(`https://${BASE_URL}/css/listing.css`, {
+        response = http.get(`${BASE_URL}/css/listing.css`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
-        response = http.get(`https://${BASE_URL}/css/l.css`, {
+        response = http.get(`${BASE_URL}/css/l.css`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
-        response = http.get(`https://${BASE_URL}/icons.svg`, {
+        response = http.get(`${BASE_URL}/icons.svg`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
-        response = http.get(`https://${BASE_URL}/js/main.js`, {
+        response = http.get(`${BASE_URL}/js/main.js`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
-        response = http.get(`https://${BASE_URL}/css/m.css`, {
+        response = http.get(`${BASE_URL}/css/m.css`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
-        response = http.get(`https://${BASE_URL}/css/s.css`, {
+        response = http.get(`${BASE_URL}/css/s.css`, {
             headers: get_headers,
         })
         check(response, {
@@ -103,13 +104,13 @@ export function regular_browsing() {
     })
 
     group('Homepage', function () {
-        response = http.get(`https://${BASE_URL}/js/main.js`, {
+        response = http.get(`${BASE_URL}/js/main.js`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
-        response = http.get('https://${BASE_URL}/icons.svg', {
+        response = http.get(`${BASE_URL}/icons.svg`, {
             headers: get_headers,
         })
         check(response, {
@@ -121,20 +122,20 @@ export function regular_browsing() {
     group(
         'Navigate next page #1',
         function () {
-            response = http.get(`https://${BASE_URL}/?after=2f5ed758-f9db-4af5-a059-2d8f3e43ab9f`, {
+            response = http.get(`${BASE_URL}/?after=2f5ed758-f9db-4af5-a059-2d8f3e43ab9f`, {
                 headers: get_headers,
             })
             check(response, {
                 'is status 200': r => r.status === 200,
             })
             sleep(1)
-            response = http.get(`https://${BASE_URL}/js/main.js`, {
+            response = http.get(`${BASE_URL}/js/main.js`, {
                 headers: get_headers,
             })
             check(response, {
                 'is status 200': r => r.status === 200,
             })
-            response = http.get(`https://${BASE_URL}/icons.svg`, {
+            response = http.get(`${BASE_URL}/icons.svg`, {
                 headers: get_headers,
             })
             check(response, {
@@ -147,20 +148,20 @@ export function regular_browsing() {
     group(
         'Navigate next page #2',
         function () {
-            response = http.get(`https://${BASE_URL}/?after=af0d8cc3-ea0e-4e08-990d-43e93b3707c6`, {
+            response = http.get(`${BASE_URL}/?after=af0d8cc3-ea0e-4e08-990d-43e93b3707c6`, {
                 headers: get_headers,
             })
             check(response, {
                 'is status 200': r => r.status === 200,
             })
             sleep(1.3)
-            response = http.get(`https://${BASE_URL}/js/main.js`, {
+            response = http.get(`${BASE_URL}/js/main.js`, {
                 headers: get_headers,
             })
             check(response, {
                 'is status 200': r => r.status === 200,
             })
-            response = http.get(`https://${BASE_URL}/icons.svg`, {
+            response = http.get(`${BASE_URL}/icons.svg`, {
                 headers: get_headers,
             })
             check(response, {
@@ -173,20 +174,20 @@ export function regular_browsing() {
     group(
         'Navigate next page #3',
         function () {
-            response = http.get(`https://${BASE_URL}/?after=a049dd28-7af4-41ac-a6ff-7f9ba62a2bad`, {
+            response = http.get(`${BASE_URL}/?after=a049dd28-7af4-41ac-a6ff-7f9ba62a2bad`, {
                 headers: get_headers,
             })
             check(response, {
                 'is status 200': r => r.status === 200,
             })
             sleep(1.3)
-            response = http.get(`https://${BASE_URL}/js/main.js`, {
+            response = http.get(`${BASE_URL}/js/main.js`, {
                 headers: get_headers,
             })
             check(response, {
                 'is status 200': r => r.status === 200,
             })
-            response = http.get(`https://${BASE_URL}/icons.svg`, {
+            response = http.get(`${BASE_URL}/icons.svg`, {
                 headers: get_headers,
             })
             check(response, {
@@ -199,20 +200,20 @@ export function regular_browsing() {
     group(
         'Navigate next page #4',
         function () {
-            response = http.get(`https://${BASE_URL}/?after=fd6f28ad-7231-4a3e-83d7-9608c0eea3f0`, {
+            response = http.get(`${BASE_URL}/?after=fd6f28ad-7231-4a3e-83d7-9608c0eea3f0`, {
                 headers: get_headers,
             })
             check(response, {
                 'is status 200': r => r.status === 200,
             })
             sleep(1.3)
-            response = http.get(`https://${BASE_URL}/js/main.js`, {
+            response = http.get(`${BASE_URL}/js/main.js`, {
                 headers: get_headers,
             })
             check(response, {
                 'is status 200': r => r.status === 200,
             })
-            response = http.get(`https://${BASE_URL}/icons.svg`, {
+            response = http.get(`${BASE_URL}/icons.svg`, {
                 headers: get_headers,
             })
             check(response, {
@@ -225,20 +226,20 @@ export function regular_browsing() {
     group(
         'Navigate next page #5',
         function () {
-            response = http.get(`https://${BASE_URL}/?after=13353393-2676-4516-9805-203b230c6a70`, {
+            response = http.get(`${BASE_URL}/?after=13353393-2676-4516-9805-203b230c6a70`, {
                 headers: get_headers,
             })
             check(response, {
                 'is status 200': r => r.status === 200,
             })
             sleep(0.7)
-            response = http.get(`https://${BASE_URL}/js/main.js`, {
+            response = http.get(`${BASE_URL}/js/main.js`, {
                 headers: get_headers,
             })
             check(response, {
                 'is status 200': r => r.status === 200,
             })
-            response = http.get(`https://${BASE_URL}/icons.svg`, {
+            response = http.get(`${BASE_URL}/icons.svg`, {
                 headers: get_headers,
             })
             check(response, {
@@ -251,26 +252,26 @@ export function regular_browsing() {
     group(
         'Navigate next page #6',
         function () {
-            response = http.get(`https://${BASE_URL}/~marius/2e6cf5a8-ffa8-4fca-b4cd-8e0c9a584a75`, {
+            response = http.get(`${BASE_URL}/~marius/2e6cf5a8-ffa8-4fca-b4cd-8e0c9a584a75`, {
                 headers: get_headers,
             })
             check(response, {
                 'is status 200': r => r.status === 200,
             })
             sleep(0.8)
-            response = http.get(`https://${BASE_URL}/css/content.css`, {
+            response = http.get(`${BASE_URL}/css/content.css`, {
                 headers: get_headers,
             })
             check(response, {
                 'is status 200': r => r.status === 200,
             })
-            response = http.get(`https://${BASE_URL}/js/main.js`, {
+            response = http.get(`${BASE_URL}/js/main.js`, {
                 headers: get_headers,
             })
             check(response, {
                 'is status 200': r => r.status === 200,
             })
-            response = http.get(`https://${BASE_URL}/icons.svg`, {
+            response = http.get(`${BASE_URL}/icons.svg`, {
                 headers: get_headers,
             })
             check(response, {
@@ -281,26 +282,26 @@ export function regular_browsing() {
     )
 
     group('Moderation', function () {
-        response = http.get(`https://${BASE_URL}/moderation`, {
+        response = http.get(`${BASE_URL}/moderation`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
         sleep(0.5)
-        response = http.get(`https://${BASE_URL}/css/moderation.css`, {
+        response = http.get(`${BASE_URL}/css/moderation.css`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
-        response = http.get(`https://${BASE_URL}/js/main.js`, {
+        response = http.get(`${BASE_URL}/js/main.js`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
-        response = http.get(`https://${BASE_URL}/icons.svg`, {
+        response = http.get(`${BASE_URL}/icons.svg`, {
             headers: get_headers,
         })
         check(response, {
@@ -313,7 +314,7 @@ export function regular_browsing() {
         'Moderation next page #1',
         function () {
             response = http.get(
-                `https://${BASE_URL}/moderation?after=f1609fe1-f46f-4f70-802f-5dd75034471f`,
+                `${BASE_URL}/moderation?after=f1609fe1-f46f-4f70-802f-5dd75034471f`,
                 {
                     headers: get_headers,
                 }
@@ -322,10 +323,10 @@ export function regular_browsing() {
                 'is status 200': r => r.status === 200,
             })
             sleep(0.7)
-            response = http.get(`https://${BASE_URL}/js/main.js`, {
+            response = http.get(`${BASE_URL}/js/main.js`, {
                 headers: get_headers,
             })
-            response = http.get(`https://${BASE_URL}/icons.svg`, {
+            response = http.get(`${BASE_URL}/icons.svg`, {
                 headers: get_headers,
             })
             check(response, {
@@ -336,20 +337,20 @@ export function regular_browsing() {
     )
 
     group('Moderation just comments', function () {
-        response = http.get(`https://${BASE_URL}/moderation?t=c`, {
+        response = http.get(`${BASE_URL}/moderation?t=c`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
         sleep(0.5)
-        response = http.get(`https://${BASE_URL}/js/main.js`, {
+        response = http.get(`${BASE_URL}/js/main.js`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
-        response = http.get(`https://${BASE_URL}/icons.svg`, {
+        response = http.get(`${BASE_URL}/icons.svg`, {
             headers: get_headers,
         })
         check(response, {
@@ -359,20 +360,20 @@ export function regular_browsing() {
     })
 
     group('Moderation just submissions', function () {
-        response = http.get(`https://${BASE_URL}/moderation?t=s`, {
+        response = http.get(`${BASE_URL}/moderation?t=s`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
         sleep(0.7)
-        response = http.get(`https://${BASE_URL}/js/main.js`, {
+        response = http.get(`${BASE_URL}/js/main.js`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
-        response = http.get(`https://${BASE_URL}/icons.svg`, {
+        response = http.get(`${BASE_URL}/icons.svg`, {
             headers: get_headers,
         })
         check(response, {
@@ -382,20 +383,20 @@ export function regular_browsing() {
     })
 
     group('Followed page', function () {
-        response = http.get(`https://${BASE_URL}/followed`, {
+        response = http.get(`${BASE_URL}/followed`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
         sleep(2.7)
-        response = http.get(`https://${BASE_URL}/js/main.js`, {
+        response = http.get(`${BASE_URL}/js/main.js`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
-        response = http.get(`https://${BASE_URL}/icons.svg`, {
+        response = http.get(`${BASE_URL}/icons.svg`, {
             headers: get_headers,
         })
         check(response, {
@@ -405,20 +406,20 @@ export function regular_browsing() {
     })
 
     group('Self page', function () {
-        response = http.get(`https://${BASE_URL}/self`, {
+        response = http.get(`${BASE_URL}/self`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
         sleep(0.5)
-        response = http.get(`https://${BASE_URL}/js/main.js`, {
+        response = http.get(`${BASE_URL}/js/main.js`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
-        response = http.get(`https://${BASE_URL}/icons.svg`, {
+        response = http.get(`${BASE_URL}/icons.svg`, {
             headers: get_headers,
         })
         check(response, {
@@ -428,19 +429,19 @@ export function regular_browsing() {
     })
 
     group('Logout', function () {
-        response = http.get(`https://${BASE_URL}/logout`, {
+        response = http.get(`${BASE_URL}/logout`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
-        response = http.get(`https://${BASE_URL}/js/main.js`, {
+        response = http.get(`${BASE_URL}/js/main.js`, {
             headers: get_headers,
         })
         check(response, {
             'is status 200': r => r.status === 200,
         })
-        response = http.get(`https://${BASE_URL}/icons.svg`, {
+        response = http.get(`${BASE_URL}/icons.svg`, {
             headers: get_headers,
         })
         check(response, {
