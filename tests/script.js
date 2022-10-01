51d6e32cd9cf131f2f8600444f02ee9c6d04d89f
Marius Orcsik <marius@habarnam.ro>
2022-10-01 16:10:16 +0200

    Adding some authenticated pages tests to the integration script

--- tests/script.js
+++ tests/script.js
@@ -1,5 +1,5 @@
-import { sleep, group, check } from 'k6';
-import { parseHTML } from 'k6/html';
+import {check, fail, group, sleep} from 'k6';
+import {parseHTML} from 'k6/html';
 import http from 'k6/http';
 
 export const options = {
@@ -9,75 +9,56 @@ export const options = {
         'http_req_duration{type:content}': ['p(95)<200'], // threshold on API requests only
         'http_req_duration{type:static}': ['p(95)<100'], // threshold on static content only
 
-  },
+    },
     scenarios: {
         RegularBrowsing: {
             executor: 'per-vu-iterations',
             vus: 2,
             iterations: 10,
             maxDuration: '1s',
-            exec: 'regular_browsing',
+            exec: 'regularBrowsing',
+        },
+        AuthenticatedBrowsing: {
+            executor: 'per-vu-iterations',
+            vus: 2,
+            iterations: 10,
+            maxDuration: '1s',
+            exec: 'authenticatedBrowsing',
         },
     },
 }
 
 const BASE_URL = `https://${__ENV.TEST_HOST}:${__ENV.TEST_PORT}`;
 
-/*
-const PASSWORD = 'Sup3rS3cretS3cr3tP4ssW0rd!';
-
 export function setup() {
-    for (let i = 1; i <= 10; i++) {
+    for (let i in users) {
+        let u = users[i];
         let response = http.get(`${BASE_URL}/register/`);
         check(response, {
                 'user was created': (r) => r.status === 200
             }
         );
 
-        let username = `user_${i}`;
         response = response.submitForm({
             formSelector: 'form',
             fields: {
-                'handle': username,
-                'pw': PASSWORD,
-                'pw-confirm': PASSWORD,
+                'handle': u.handle,
+                'pw': u.pw,
+                'pw-confirm': u.pw,
             },
         });
         check(response, {
             'is status 200': r => r.status === 200,
         })
 
-        response = http.get(`${BASE_URL}/~${username}`);
+        response = http.get(`${BASE_URL}/~${u.handle}`);
         check(response, {
             'is status 200': r => r.status === 200,
         });
     }
 }
 
-function authenticate() {
-    let response = http.get(`${BASE_URL}/login`);
-    check(response, {
-            'login page': (r) => r.status === 200
-        }
-    );
-
-    response = response.submitForm({
-        formSelector: 'form',
-        fields: {
-            'handle': "marius",
-            'pw': "asd",
-        },
-    });
-    check(response, {
-        'is status 200': r => r.status === 200,
-        'has auth cookie': r => function (r) {
-            console.log(r);
-        },
-    })
-}
-*/
-
-const anonymousBrowsing = {
+const anonymousPages = {
     'About': {
         'path': '/about',
         'title': 'About',
@@ -116,33 +97,14 @@ const anonymousBrowsing = {
     },
 };
 
-const authenticatedBrowsing = {
-    'Moderation': {
-        'path': '/moderation',
-        'title': 'Moderation log',
-    },
-    'Followed tab': {
-        'path': '/followed',
-        'title': 'Followed items',
-    },
-    'User page %USERNAME%': {
-        'path': '/~%USERNAME%',
-        'title': '%USER%&#39;s submissions',
-    },
-    'New submission page': {
-        'path': '/submit',
-        'title': 'Add new submission',
-    },
-};
-
 function checkAssets(doc) {
     let styles = doc.find('link[rel=stylesheet]');
     styles.map(
         function (idx, el) {
             group(`stylesheets[${styles.size()}]`, function () {
                 let styleURL = el.attr('href');
-                group(`${styleURL}`, function() {
-                    let response = http.get(`${BASE_URL}${styleURL}`, {tags: { type: 'static' }})
+                group(`${styleURL}`, function () {
+                    let response = http.get(`${BASE_URL}${styleURL}`, {tags: {type: 'static'}})
                     check(response, {
                         'OK': (r) => r.status === 200 && r.headers["Content-Type"] === 'text/css; charset=utf-8',
                     });
@@ -151,12 +113,12 @@ function checkAssets(doc) {
         }
     );
 
-    let scripts  = doc.find('script[src]');
+    let scripts = doc.find('script[src]');
     scripts.map((idx, el) => {
         group(`scripts[${scripts.size()}]`, function () {
             let scriptURL = el.attr('src');
-            group(`${scriptURL}`, function() {
-                let response = http.get(`${BASE_URL}${scriptURL}`, {tags: { type: 'static' }})
+            group(`${scriptURL}`, function () {
+                let response = http.get(`${BASE_URL}${scriptURL}`, {tags: {type: 'static'}})
                 check(response, {
                     'OK': (r) => r.status === 200 && r.headers["Content-Type"] === 'text/javascript; charset=utf-8',
                 });
@@ -165,53 +127,100 @@ function checkAssets(doc) {
     });
 }
 
-export function regular_browsing() {
+function pageAssertions(test) {
+    return function () {
+        let response = http.get(`${BASE_URL}${test.path}`, {tags: {type: 'content'}});
+        check(response, {
+            'is status 200': r => r.status === 200,
+        });
+
+        const doc = parseHTML(response.body);
+        // Check title matches
+        check(doc.find('head title').text(), {
+            'has correct title': (s) => s === test.title,
+        });
+        checkAssets(doc);
+        sleep(0.1);
+    }
+};
+
+export function regularBrowsing() {
     group('/icons.svg', function () {
-        let response = http.get(`${BASE_URL}/icons.svg`, {tags: { type: 'static' }})
+        let response = http.get(`${BASE_URL}/icons.svg`, {tags: {type: 'static'}})
         check(response, {
             'is status 200': (r) => r.status === 200,
             'is svg': (r) => r.headers["Content-Type"] === 'image/svg+xml',
         });
     });
 
-    for (let m in anonymousBrowsing) {
-        let test = anonymousBrowsing[m];
-        group(m, function () {
-            let response = http.get(`${BASE_URL}${test.path}`, {tags: { type: 'content' }});
-            check(response, {
-                'is status 200': r => r.status === 200,
-            });
-
-            const doc = parseHTML(response.body);
-            // Check title matches
-            check(doc.find('head title').text(), {
-                'has correct title': (s) => s === test.title,
-            });
-            checkAssets(doc);
-            sleep(0.1);
-        });
+    for (let m in anonymousPages) {
+        let test = anonymousPages[m];
+        group(m, pageAssertions(test));
     }
+};
 
-
-    /*
-    authenticate();
-
-    for (let m in authenticatedBrowsing) {
-        let test = authenticatedBrowsing[m];
-        group(m, function () {
-            let response = http.get(`${BASE_URL}${test.path}`, {tags: { type: 'content' }});
-            check(response, {
-                'is status 200': r => r.status === 200,
-            });
-
-            const doc = parseHTML(response.body);
-            // Check title matches
-            check(doc.find('head title').text(), {
-                'has correct title': (s) => s === test.title,
-            });
-            checkAssets(doc);
-            sleep(0.1);
-        });
+const PASSWORD = 'Sup3rS3cretS3cr3tP4ssW0rd!';
+const users = [
+    {
+        handle: 'user_test_0',
+        pw: PASSWORD,
     }
-    */
+];
+
+const authenticatedPages = {
+    'Moderation': {
+        path: '/moderation',
+        title: 'Moderation log',
+        user: users[0],
+    },
+    'Followed tab': {
+        path: '/followed',
+        title: 'Followed items',
+        user: users[0],
+    },
+    'User page': {
+        path: `/~${users[0].handle}`,
+        title: `${users[0].handle}&#39;s submissions`,
+        user: users[0],
+    },
+    'New submission page': {
+        path: '/submit',
+        title: 'Add new submission',
+        user: users[0],
+    },
+};
+
+function authenticate(u) {
+    let response = http.get(`${BASE_URL}/login`);
+    check(response, {
+            'login page': (r) => r.status === 200
+        }
+    );
+
+    response = response.submitForm({
+        formSelector: 'form',
+        fields: u,
+    });
+    check(response, {
+        'is status 200': r => r.status === 200,
+        'has auth cookie': r => function (r) {
+            console.log(r);
+        },
+    })
+}
+
+export function authenticatedBrowsing() {
+    for (let m in authenticatedPages) {
+        let test = authenticatedPages[m];
+
+        if (test.user == null) {
+            fail("invalid user");
+            return;
+        }
+        let u = test.user;
+
+        authenticate(u);
+
+        group(`${m}: ${u.handle}`, pageAssertions(test));
+    }
 };
