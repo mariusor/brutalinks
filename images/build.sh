6c5cd545053a0913455064a5bb7a7a95b58d8ee0
Marius Orcsik <marius@federated.id>
2023-02-25 17:29:20 +0100

    Updated builder image to go1.20

--- images/build.sh
+++ images/build.sh
@@ -7,22 +7,15 @@ _image_name=${2:-brutalinks/builder}
 
 _context=$(realpath "${_workdir}")
 
-#FROM golang:1.19
-_builder=$(buildah from docker.io/library/golang:1.19)
+_builder=$(buildah from docker.io/library/golang:1.20)
 
-#ENV GO111MODULE=on
 buildah config --env GO111MODULE=on "${_builder}"
-#ENV GOWORK=off
 buildah config --env GOWORK=off "${_builder}"
 
-#ADD ./ /go/src/app
-#_context=$(realpath ../)
 buildah copy --ignorefile "${_context}/.containerignore" --contextdir "${_context}" "${_builder}" "${_context}" /go/src/app
 
-#WORKDIR /go/src/app
 buildah config --workingdir /go/src/app "${_builder}"
 
-#RUN make download && go mod vendor
 buildah run "${_builder}" make download
 buildah run "${_builder}" go mod vendor
 
