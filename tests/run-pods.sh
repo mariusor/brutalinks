efac2f87d52accb8b8ddb118500644d62ba47c43
Marius Orcsik <marius@federated.id>
2024-02-26 21:45:46 +0100

    Added caddy to the integration tests setup

--- tests/run-pods.sh
+++ tests/run-pods.sh
@@ -5,36 +5,23 @@ set -e
 TEST_PORT=${TEST_PORT:-4499}
 IMAGE=${IMAGE:-quay.io/go-ap/brutalinks:qa}
 
-if podman container exists tests_fedbox; then
-    podman stop tests_fedbox
-    podman rm -f tests_fedbox
-fi
-if podman container exists tests_fedbox_auth; then
-    podman stop tests_fedbox_auth
-    podman rm -f tests_fedbox_auth
-fi
-if podman container exists tests_brutalinks; then
-    podman stop tests_brutalinks
-    podman rm -f tests_brutalinks
-fi
-
 if podman network exists tests_network; then
     podman network rm -f tests_network
 fi
 
 podman network create tests_network
 
-podman run -d \
+podman run -d --replace \
     --pull newer \
     --name=tests_fedbox \
     -v $(pwd)/fedbox/env:/.env \
     -v $(pwd)/fedbox:/storage \
     -e ENV=test \
-    -e LISTEN=:443 \
+    -e LISTEN=:8443 \
     -e HOSTNAME=fedbox \
     --net tests_network \
-    --network-alias fedbox \
-    --expose 443 \
+    --network-alias fedbox_pub \
+    --expose 8443 \
     quay.io/go-ap/fedbox:qa-fs \
     /bin/fedbox
 
@@ -44,15 +31,15 @@ if [ -s ${_fedbox_running} ]; then
     exit 1
 fi
 
-podman run -d \
+podman run -d --replace \
     --pull newer \
     --name=tests_fedbox_auth \
     -v $(pwd)/fedbox:/storage \
     --net tests_network \
-    --network-alias fedbox-auth \
-    --expose 444 \
+    --network-alias fedbox_auth \
+    --expose 8443 \
     quay.io/go-ap/auth:qa \
-    --env test --listen :444 --storage fs:///storage/%storage%/%env%
+    --env test --listen :8443 --storage fs:///storage/%storage%/%env%
 
 _fedbox_auth_running=$(podman ps --filter name=tests_fedbox_auth --format '{{ .Names }}' )
 if [ -s ${_fedbox_auth_running} ]; then
@@ -60,7 +47,21 @@ if [ -s ${_fedbox_auth_running} ]; then
     exit 1
 fi
 
-podman run -d \
+podman run --replace -d \
+    -p ${TEST_PORT}:443 \
+    --name=tests_caddy \
+    -v $(pwd)/Caddyfile:/etc/caddy/Caddyfile \
+    -v caddy_data:/data \
+    --expose 443 \
+    caddy
+_caddy_running=$(podman ps --filter name=tests_caddy --format '{{ .Names }}' )
+if [ -s ${_caddy_running} ]; then
+    echo "Unable to run test pod for Caddy"
+    exit 1
+fi
+
+set -x
+podman run -d --replace \
     --pull newer \
     --name=tests_brutalinks \
     -v $(pwd)/brutalinks/env:/.env \
@@ -68,8 +69,7 @@ podman run -d \
     -e LISTEN_HOST= \
     --net tests_network \
     --network-alias brutalinks \
-    --expose 443 \
-    -p "${TEST_PORT}:443" \
+    --expose 8443 \
     "${IMAGE}" \
     /bin/brutalinks
 
