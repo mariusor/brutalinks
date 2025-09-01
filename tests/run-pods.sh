ab8072dcb09452319f6d0a2364997479e902052c
Marius Orcsik <marius@federated.id>
2025-09-01 10:47:42 +0200

    Try to make integration tests work again

--- tests/run-pods.sh
+++ tests/run-pods.sh
@@ -2,35 +2,40 @@
 
 set -e
 
-ENV=${ENV:-dev}
 TEST_PORT=${TEST_PORT:-4499}
 AUTH_IMAGE=${AUTH_IMAGE:-localhost/auth/app:dev-fs}
+POINT_IMAGE=${POINT_IMAGE:-localhost/point/app:dev-fs}
 FEDBOX_IMAGE=${FEDBOX_IMAGE:-localhost/fedbox/app:dev-fs}
-IMAGE=${IMAGE:-localhost/brutalinks/app:${ENV}}
+IMAGE=${IMAGE:-localhost/brutalinks/app:dev}
 NETWORK=${NETWORK:-tests_network}
 
 if [ "${NETWORK}" != "host" ]; then
-    if podman network exists ${NETWORK}; then
-        podman network rm -f ${NETWORK}
+    if podman network exists "${NETWORK}"; then
+        podman network rm -f "${NETWORK}"
     fi
 
     podman network create --subnet 10.6.6.0/24 --gateway 10.6.6.1 "${NETWORK}"
 fi
 
+## FedBOX
+
+if [ "${NETWORK}" != "host" ]; then
+  FEDBOX_NETWORK="--network-alias fedbox-internal --ip 10.6.6.61 "
+fi
 podman run -d --replace \
     --pull newer \
     --name=tests_fedbox \
-    -v $(pwd)/mocks/fedbox/env:/.env \
-    -v $(pwd)/mocks/fedbox:/storage \
+    -v "$(pwd)/mocks/fedbox/env:/.env" \
+    -v "$(pwd)/mocks/fedbox:/storage" \
     -e ENV=test \
     -e STORAGE=fs \
     -e LISTEN=:8443 \
     -e HOSTNAME=fedbox \
     --net "${NETWORK}" \
-    --network-alias fedbox-internal \
-    --ip 10.6.6.61 \
     --expose 8443 \
-    ${FEDBOX_IMAGE}
+    ${FEDBOX_NETWORK} \
+    "${FEDBOX_IMAGE}" \
+    --env test
 
 _fedbox_running=$(podman ps --filter name=tests_fedbox --format '{{ .Names }}')
 if [ -z "${_fedbox_running}" ]; then
@@ -39,15 +44,19 @@ if [ -z "${_fedbox_running}" ]; then
     exit 1
 fi
 
+## OAuth2
+
+if [ "${NETWORK}" != "host" ]; then
+  AUTH_NETWORK="--net ${NETWORK} --network-alias auth-internal --ip 10.6.6.62"
+fi
+
 podman run -d --replace \
     --pull newer \
     --name=tests_auth \
-    -v $(pwd)/mocks/fedbox:/storage \
-    --net "${NETWORK}" \
-    --network-alias auth-internal \
-    --ip 10.6.6.62 \
+    -v "$(pwd)/mocks/fedbox:/storage" \
     --expose 8080 \
-    ${AUTH_IMAGE} \
+    ${AUTH_NETWORK} \
+    "${AUTH_IMAGE}" \
     --env test --listen :8080 --storage fs:///storage/%storage%/%env%
 
 _auth_running=$(podman ps --filter name=tests_auth --format '{{ .Names }}')
@@ -57,18 +66,37 @@ if [ -z "${_auth_running}" ]; then
     exit 1
 fi
 
+## WebFinger
+
+if [ "${NETWORK}" != "host" ]; then
+  POINT_NETWORK="--net ${NETWORK} --network-alias point-internal --ip 10.6.6.63"
+fi
+
+podman run -d --replace \
+    --pull newer \
+    --name=tests_point \
+    -v "$(pwd)/mocks/fedbox:/storage" \
+    --expose 8080 \
+    ${POINT_NETWORK} \
+    "${POINT_IMAGE}" \
+    --env test --listen :8080 --storage fs:///storage/%storage%/%env% --root https://fedbox
+
+_point_running=$(podman ps --filter name=tests_point --format '{{ .Names }}')
+if [ -z "${_point_running}" ]; then
+    echo "Unable to run test fedbox WebFinger pod: ${POINT_IMAGE}"
+    podman logs tests_point
+    exit 1
+fi
+
+if [ "${NETWORK}" != "host" ]; then
+  CADDY_NETWORK="--net ${NETWORK} --network-alias fedbox --network-alias brutalinks --network-alias auth --network-alias point --ip 10.6.6.6 --expose 443 --expose 80"
+fi
 podman run --replace -d \
-    -p ${TEST_PORT}:443 \
+    -p "${TEST_PORT}":443 \
     --name=tests_caddy \
-    -v $(pwd)/mocks/Caddyfile:/etc/caddy/Caddyfile \
+    -v "$(pwd)/mocks/Caddyfile:/etc/caddy/Caddyfile" \
     -v caddy_data:/data \
-    --net "${NETWORK}" \
-    --network-alias fedbox \
-    --network-alias brutalinks \
-    --network-alias auth \
-    --ip 10.6.6.6 \
-    --expose 443 \
-    --expose 80 \
+    ${CADDY_NETWORK} \
     docker.io/library/caddy:2
 
 _caddy_running=$(podman ps --filter name=tests_caddy --format '{{ .Names }}')
@@ -78,18 +106,17 @@ if [ -z "${_caddy_running}" ]; then
     exit 1
 fi
 
+if [ "${NETWORK}" != "host" ]; then
+  BRUTALINKS_NETWORK="--net ${NETWORK} --network-alias brutalinks-internal --add-host fedbox:10.6.6.6 --add-host auth:10.6.6.6 --ip 10.6.6.66"
+fi
 podman run -d --replace \
     --pull newer \
     --name=tests_brutalinks \
-    -v $(pwd)/mocks/brutalinks/env:/.env \
-    -v $(pwd)/mocks/brutalinks:/storage \
+    -v "$(pwd)/mocks/brutalinks/env:/.env" \
+    -v "$(pwd)/mocks/brutalinks:/storage" \
     -e LISTEN_HOST=brutalinks \
-    --net "${NETWORK}" \
-    --network-alias brutalinks-internal \
-    --add-host fedbox:10.6.6.6 \
-    --add-host auth:10.6.6.6 \
-    --ip 10.6.6.66 \
     --expose 8443 \
+    ${BRUTALINKS_NETWORK} \
     "${IMAGE}"
 sleep 1
 
@@ -101,4 +128,3 @@ if [ -z "${_brutalinks_running}" ]; then
 fi
 echo "Brutalinks pod running: ${IMAGE}"
 sleep 2
-
