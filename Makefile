b1a82d8aff6f55e2e4c03808ab4d1a2b5bba36c7
Marius Orcsik <marius@federated.id>
2025-12-26 17:38:50 +0100

    Move to the new way of building the container image

--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@ MAKEFLAGS += --warn-undefined-variables
 MAKEFLAGS += --no-builtin-rules
 
 PROJECT_NAME := brutalinks
+APP_HOSTNAME ?= $(PROJECT_NAME)
 ENV ?= dev
 
 LDFLAGS ?= -X main.version=$(VERSION)
@@ -18,11 +19,7 @@ APPSOURCES := $(wildcard ./*.go internal/*/*.go cmd/brutalinks/*.go)
 ASSETFILES := $(wildcard assets/*/* assets/*)
 
 export CGO_ENABLED=0
-
-ifneq ($(ENV), dev)
-	LDFLAGS += -s -w -extldflags "-static"
-	BUILDFLAGS += -trimpath
-endif
+export GOEXPERIMENT=greenteagc
 
 ifeq ($(shell git describe --always > /dev/null 2>&1 ; echo $$?), 0)
 	BRANCH=$(shell git rev-parse --abbrev-ref HEAD | tr '/' '-')
@@ -33,48 +30,51 @@ ifeq ($(shell git describe --tags > /dev/null 2>&1 ; echo $$?), 0)
 	VERSION ?= $(shell git describe --tags | tr '/' '-')
 endif
 
+ifneq ($(ENV), dev)
+	LDFLAGS += -s -w -extldflags "-static"
+	BUILDFLAGS += -trimpath
+endif
+
 BUILD := $(GO) build $(BUILDFLAGS)
 TEST := $(GO) test $(BUILDFLAGS)
 
-.PHONY: all brutalinks download run clean images test assets help
+.PHONY: all cert brutalinks clean test images test assets download help
 
 .DEFAULT_GOAL := help
 
 help: ## Help target that shows this message.
 	@sed -rn 's/^([^:]+):.*[ ]##[ ](.+)/\1:\2/p' $(MAKEFILE_LIST) | column -ts: -l2
 
-all: brutalinks
+all: brutalinks ##
 
-download: go.sum
+download: go.sum ## Downloads dependencies and tidies the go.mod file.
 
 go.sum: go.mod
 	$(GO) mod download all
 	$(GO) mod tidy
 
 brutalinks: bin/brutalinks ## Builds the brutalinks binary.
-
-ifneq ($(ENV),dev)
-assets:  internal/assets/assets.gen.go
-else
-assets: ## Builds static javascript and css files for embedding in the production binaries.
-endif
-
-internal/assets/assets.gen.go: $(ASSETFILES)
-	$(GO) run -tags $(ENV) ./internal/assets/cmd/minify.go -build "prod || qa" -glob assets/*,assets/css/*,assets/js/*,README.md -var AssetFS -o ./internal/assets/assets.gen.go
-
 bin/brutalinks: go.mod go.sum $(APPSOURCES) assets
 	$(BUILD) -tags $(ENV) -o $@ ./cmd/brutalinks
 ifneq ($(ENV),dev)
 	$(UPX) -q --mono --no-progress --best $@ || true
 endif
 
-run: ./bin/brutalinks ## Runs the brutalinks binary.
-	@./bin/brutalinks
+assets: ## Builds static javascript and css files for embedding in the production binaries.
+ifneq ($(ENV),dev)
+assets: internal/assets/assets.gen.go
+endif
+
+internal/assets/assets.gen.go: $(ASSETFILES)
+	$(GO) run -tags $(ENV) ./internal/assets/cmd/minify.go -build "prod || qa" -glob assets/*,assets/css/*,assets/js/*,README.md -var AssetFS -o ./internal/assets/assets.gen.go
+
+cert: bin/$(APP_HOSTNAME).pem ## Create a certificate.
+bin/$(APP_HOSTNAME).pem: bin/$(APP_HOSTNAME).key bin/$(APP_HOSTNAME).crt
+bin/$(APP_HOSTNAME).key bin/$(APP_HOSTNAME).crt:
+	./tools/gen-certs.sh ./bin/$(APP_HOSTNAME)
 
 clean: ## Cleanup the build workspace.
-	-$(RM) bin/* internal/assets/*.gen.go
-	$(GO) build clean
-	$(MAKE) -C images $@
+	-$(RM) -r bin/* internal/assets/*.gen.go
 
 images: ## Build podman images.
 	$(MAKE) -C images $@
@@ -83,12 +83,11 @@ test: TEST_TARGET := . ./internal/...
 test: download go.sum ## Run unit tests for the service.
 	$(TEST) $(TEST_FLAGS) $(TEST_TARGET)
 
-coverage: TEST_TARGET := .
-coverage: TEST_FLAGS += -covermode=count -coverprofile $(PROJECT_NAME).coverprofile
-coverage: test ## Run unit tests for the service with coverage.
+coverage: integration ## Run unit tests for the service with coverage.
+	$(GO) tool covdata percent -i=./tests/.cache -o $(PROJECT).coverprofile
 
 integration: download ## Run integration tests for the service.
 	if [ -z "${IMAGE}" ]; then
 		make ENV=dev -C images builder build
 	fi
-	make -C tests pods test-podman
+	make -C tests test
--- images/image.sh
+++ images/image.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-#set -x
+#set -ex
+set -e
+_context=$(realpath "../")
 
 _environment=${ENV:-dev}
 _hostname=${APP_HOSTNAME:-brutalinks}
@@ -8,26 +10,64 @@ _listen_port=${PORT:-3003}
 _version=${VERSION:-HEAD}
 
 _image_name=${1:-brutalinks:${_environment}}
-_build_name=${2:-localhost/brutalinks/builder}
-
-_builder=$(buildah from "${_build_name}":latest)
-if [[ -z "${_builder}" ]]; then
-    echo "Unable to find builder image: ${_build_name}"
-    exit 1
-fi
 
 echo "Building image ${_image_name} for host:${_hostname} env:${_environment} port:${_listen_port} version:${_version}"
 
-buildah run "${_builder}" make ENV="${_environment}" VERSION="${_version}" all
-buildah run "${_builder}" ./images/gen-certs.sh ${_hostname}
+HOST_GOCACHE=$(go env GOCACHE)
+HOST_GOMODCACHE=$(go env GOMODCACHE)
+
+GOCACHE=/root/.cache/go-build
+GOMODCACHE=/go/pkg/mod
+
+_builder=$(buildah from docker.io/library/golang:1.25-alpine)
+
+buildah run "${_builder}" /sbin/apk update
+buildah run "${_builder}" /sbin/apk add make bash openssl upx
+
+buildah config --env GO111MODULE=on "${_builder}"
+buildah config --env GOWORK=off "${_builder}"
+buildah config --env "GOCACHE=${GOCACHE}" "${_builder}"
+buildah config --env "GOMODCACHE=${GOMODCACHE}" "${_builder}"
+
+buildah config --workingdir /go/src/app "${_builder}"
+
+buildah run \
+    --mount="type=bind,rw,source=${HOST_GOCACHE},destination=${GOCACHE}" \
+    --mount="type=bind,rw,source=${HOST_GOMODCACHE},destination=${GOMODCACHE}" \
+    --mount="type=bind,rw,source=${_context},destination=/go/src/app" \
+    --mount=type=cache,rw,id=bin,target=/go/src/app/bin "${_builder}" \
+    make ENV="${_environment}" VERSION="${_version}" APP_HOSTNAME="${_hostname}" clean all cert
+
+if [ "${_environment}" = "dev" ]; then
+    buildah run \
+        --mount="type=bind,rw,source=${_context},destination=/go/src/app" \
+        --mount=type=cache,rw,id=bin,target=/go/src/app/bin \
+        "${_builder}" \
+        cp -r /go/src/app/templates /go/src/app/bin
+    buildah run \
+        --mount="type=bind,rw,source=${_context},destination=/go/src/app" \
+        --mount=type=cache,rw,id=bin,target=/go/src/app/bin \
+        "${_builder}" \
+        cp -r /go/src/app/assets /go/src/app/bin
+    buildah run \
+        --mount="type=bind,rw,source=${_context},destination=/go/src/app" \
+        --mount=type=cache,rw,id=bin,target=/go/src/app/bin \
+        "${_builder}" \
+        cp -r /go/src/app/README.md /go/src/app/bin
+fi
+
+# copy binaries from cache to builder container fs
+buildah run \
+    --mount=type=cache,rw,id=bin,target=/tmp/bin "${_builder}" \
+    cp -r /tmp/bin /go/src/
 
 _image=$(buildah from gcr.io/distroless/static:latest)
 
-buildah config --env ENV="${_environment}" "${_image}"
-buildah config --env APP_HOSTNAME="${_hostname}" "${_image}"
-buildah config --env LISTEN=:"${_listen_port}" "${_image}"
-buildah config --env KEY_PATH="/etc/ssl/certs/${_hostname}.key" "${_image}"
-buildah config --env CERT_PATH="/etc/ssl/certs/${_hostname}.crt" "${_image}"
+buildah config --env "ENV=${_environment}" "${_image}"
+buildah config --env "APP_HOSTNAME=${_hostname}" "${_image}"
+buildah config --env "LISTEN=:${_listen_port}" "${_image}"
+buildah config --env "KEY_PATH=/etc/ssl/certs/${_hostname}.key" "${_image}"
+buildah config --env "CERT_PATH=/etc/ssl/certs/${_hostname}.crt" "${_image}"
 buildah config --env HTTPS=true "${_image}"
 
 buildah config --port "${_listen_port}" "${_image}"
@@ -35,15 +75,15 @@ buildah config --port "${_listen_port}" "${_image}"
 buildah config --volume /storage "${_image}"
 buildah config --volume /.env "${_image}"
 
-buildah copy --from "${_builder}" "${_image}" /go/src/app/bin/* /bin/
-buildah copy --from "${_builder}" "${_image}" "/go/src/app/${_hostname}.key" /etc/ssl/certs/
-buildah copy --from "${_builder}" "${_image}" "/go/src/app/${_hostname}.crt" /etc/ssl/certs/
-buildah copy --from "${_builder}" "${_image}" "/go/src/app/${_hostname}.pem" /etc/ssl/certs/
+buildah copy --from "${_builder}" "${_image}" /go/src/bin/brutalinks /bin/
+buildah copy --from "${_builder}" "${_image}" "/go/src/bin/${_hostname}.key" /etc/ssl/certs/
+buildah copy --from "${_builder}" "${_image}" "/go/src/bin/${_hostname}.crt" /etc/ssl/certs/
+buildah copy --from "${_builder}" "${_image}" "/go/src/bin/${_hostname}.pem" /etc/ssl/certs/
 
 if [ "${_environment}" = "dev" ]; then
-  buildah copy --from "${_builder}" "${_image}" /go/src/app/templates /templates
-  buildah copy --from "${_builder}" "${_image}" /go/src/app/assets /assets
-  buildah copy --from "${_builder}" "${_image}" /go/src/app/README.md /README.md
+  buildah copy --from "${_builder}" "${_image}" /go/src/bin/templates /templates
+  buildah copy --from "${_builder}" "${_image}" /go/src/bin/assets /assets
+  buildah copy --from "${_builder}" "${_image}" /go/src/bin/README.md /README.md
 
   buildah config --workingdir / "${_image}"
 fi
